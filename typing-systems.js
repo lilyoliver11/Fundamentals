a7ab655f37b2f6270949778298458dd6d404036a
Lily Oliver <loliver6@cnm.edu>
2021-03-30 11:52:56 -0600

    collapsed variables/data into one file and  added conditionals/functions file

--- typing-systems.js
+++
@@ -1,8 +0,0 @@
-/*
-*Variables, Data Types, and Typing
- */
-let var1 = "2";
-let var2 = 5;
-let result = var1 + var2;
-
-console.log(result);
--- variables-and-data.js
+++ variables-and-data.js
@@ -1,7 +1,7 @@
 /*
 *Variables, Data Types, and Typing
  */
-let wordData;
+/* let wordData;
 
 wordData = "Word list";
 
@@ -9,4 +9,27 @@ console.log(wordData);
 
 let subTotal = 100;
 let tax = subTotal * 0.07;
-let totalWithTax = tax + subTotal;
+let totalWithTax = tax + subTotal; */
+/* let var1 = "2";
+let var2 = 5;
+let result = var1 + var2;
+
+console.log(result); */
+/* let add = 1 + 2;
+let sub = 2 - 1;
+let mult = 2 * 4;
+let division = 4 / 2;
+let mod = 5 % 2;
+
+let string = "String 1 " + "String 2";
+
+console.log(string); */
+/* let obj = {
+    key1: "value 1",
+    key2: 3,
+    obj2: {
+        obj2key1: "value of inner object",
+    },
+};
+
+console.log(obj.obj2.obj2key1); */
