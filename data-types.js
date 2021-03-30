287116cec4fab660f692c6bd480ef06b4347c741
Lily Oliver <loliver6@cnm.edu>
2021-03-29 23:20:20 -0600

    Added variables/data, data types, typing systems, and operators JS files

---
+++ data-types.js
@@ -0,0 +1,12 @@
+/*
+*Variables, Data Types, and Typing
+ */
+let obj = {
+    key1: "value 1",
+    key2: 3,
+    obj2: {
+        obj2key1: "value of inner object",
+    },
+};
+
+console.log(obj.obj2.obj2key1);
---
+++ typing-systems.js
@@ -0,0 +1,8 @@
+/*
+*Variables, Data Types, and Typing
+ */
+let var1 = "2";
+let var2 = 5;
+let result = var1 + var2;
+
+console.log(result);
